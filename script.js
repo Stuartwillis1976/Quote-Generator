0e2a16ad5a2395c996a835883e18b9090ebb4b5d
Stuartwillis1976 <stuartwillis1976@gmail.com>
2022-01-05 10:09:08 -0500

    all done

---
+++ script.js
@@ -0,0 +1,38 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+let apiQuotes = [];
+
+//Show New Quote
+
+function newQuote() {
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    authorText.textContent = quote.author;
+    quoteText.textContent = quote.text;
+    complete();
+}
+
+// Get Quotes from API
+async function getQuotes() {
+    const apiUrl = "https://type.fit/api/quotes";
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        // Catch Error here
+    }
+}
+//To Tweet
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, "_blank");
+}
+
+// Event listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
+getQuotes();
